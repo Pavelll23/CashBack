3641725a1d92428a7f6a206f4395a05f2d8d7432
Pavel Kozlov <cozlov.pavel@mail.ru>
2023-03-05 21:56:55 +0300

    fix

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,19 +1,21 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
-    @Test
+
+    CashbackHackService service =new CashbackHackService();
+
+    @org.testng.annotations.Test
     public void testRemain() {
         int actual = service.remain(800);
         int expected = 200;
 
         assertEquals(actual, expected);
     }
-    @org.junit.Test
+    @Test
     public void shouldAmountMore1000(){
         int actual = service.remain(1100);
         int expected = 900;
